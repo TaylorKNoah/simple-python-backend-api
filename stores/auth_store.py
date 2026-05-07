00a75a6fcfc0d6c4e23bc642e63ec8772f3b5a1a
Taylor Noah <taylorknoah@gmail.com>
2026-05-06 21:55:22 -0700

    remove error

--- stores/auth_store.py
+++ stores/auth_store.py
@@ -4,7 +4,6 @@ from stores.base_store import BaseStore
 class AuthStore(BaseStore):
     def create_user(self, user: User) -> None:
         self.add(user)
-        sdfsdf
     
     def get_user_by_name(self, name: str) -> User | None:
         return User.query.filter_by(name=name).first()
